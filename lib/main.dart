18cee617234947aa0ad456845ff24e6ff1998048
pavel-shubin <52544144+pavel-shubin@users.noreply.github.com>
2024-09-16 00:16:52 +0300

    Added new StatelessWidget FavoritePage that realised the working of the same name page. Now by the click to Favorite button displayed the corresponding page

--- lib/main.dart
+++ lib/main.dart
@@ -59,12 +59,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
+
     Widget page;
       switch(selectedIndex) {
         case 0:
         page = GeneratorPage();
         case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         default:
         throw UnimplementedError('no widget for $selectedIndex');
       }
@@ -183,5 +184,31 @@ class BigCard extends StatelessWidget {
           ),
       ),
     );
+  }
+}
+
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text('No favorites yet.'),);
+    }
+
+    return ListView(
+    children: [
+      Padding(padding: const EdgeInsets.all(20),
+      child: Text('You have '
+      '${appState.favorites.length} favorites:'),
+      ),
+      for (var pair in appState.favorites) 
+      ListTile(
+        leading: Icon(Icons.favorite),
+        title: Text(pair.asLowerCase),
+      ),
+    ],
+  );
   }
 }
